9ded5c5109843a81b32097967890d105e13ec975
Shimaa Mahmoud <shimaahriaz@gmail.com>
2026-05-17 22:15:52 +0300

    A simple command-line task manager built with Python.

---
+++ Readmy.md
@@ -0,0 +1,19 @@
+# To-Do List CLI
+
+A simple command-line task manager built with Python.
+
+## Features
+- Add tasks
+- Show all tasks
+- Mark tasks as completed
+- Delete tasks
+- Save tasks to a file (tasks persist after closing)
+
+## How to run
+1. Make sure Python is installed
+2. Clone this repository
+3. Run the file:
+python todo.py
+
+## Tech used
+- Python (no frameworks, no external libraries)
---
+++ test.py
@@ -0,0 +1,69 @@
+tasks = [];
+
+def show_menu():
+    print("/n--- Task Manager---/n")
+    print("1. Add task")
+    print("2. Show tasks")
+    print("3. Mark task as completed")
+    print("4. Delete task")
+    print("5. Exit")
+
+
+def add_task():
+    task = input("Enter the task: ")
+    tasks.append({"task": task, "completed": False})
+    print(f"Task '{task}' added successfully!")
+
+def show_tasks():
+    if not tasks:
+        print("No tasks to show.")
+    else:
+        for index, task in enumerate(tasks):
+            status = "Completed" if task["completed"] else "Pending"
+            print(f"{index + 1}. {task['task']} - {status}")
+
+
+def mark_task_completed():
+    show_tasks()
+    if not tasks:
+        return
+    try:
+        task_number = int(input("Enter the task number to mark as completed: "))
+        if 0 < task_number <= len(tasks):
+            tasks[task_number - 1]["completed"] = True
+            print(f"Task '{tasks[task_number - 1]['task']}' marked as completed!")
+    except ValueError:
+        print("Please enter a valid task number.")
+
+
+
+def delete_task():
+    show_tasks()
+    if not tasks:
+        return
+    try:
+        task_number = int(input("Enter the task number to delete: "))
+        if 0 < task_number <= len(tasks):
+            removed_task = tasks.pop(task_number - 1)
+            print(f"Task '{removed_task['task']}' deleted successfully!")
+    except ValueError:
+        print("Please enter a valid task number.")
+
+
+
+while True:
+    show_menu()
+    choice = input("Enter your choice: ")
+    if choice == '1':
+        add_task()
+    elif choice == '2':
+        show_tasks()
+    elif choice == '3':
+        mark_task_completed()
+    elif choice == '4':
+        delete_task()
+    elif choice == '5':
+        print("Exiting Task Manager. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
